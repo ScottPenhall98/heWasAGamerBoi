55b6d086bbba59e54728b7ecf0d4edefc95b5238
ScottPenhall98 <scottpenhall98@gmail.com>
2020-07-24 23:36:32 +1200

    adding percentages in. notes in notebook

--- functions.js
+++ functions.js
@@ -6,20 +6,20 @@
 //make a graph that displays all data by a given platform
 
 //remove pal
-var myData = data
-var displayObject = {}
-var allConsoles = []
-var canvasWidth = 1600
-var canvasHeight = 800
+const MyData = data
+let displayObject = {}
+let allConsoles = []
+const CanvasWidth = 1740
+const CanvasHeight = 800
 
 function getAllConsoles(){
-    for (var i = 0; i < data.length; i ++){
-        var currentConsole = myData[i]["console-name"]
+    for (let i = 0; i < data.length; i ++){
+        let currentConsole = MyData[i]["console-name"]
         splitArray = currentConsole.split("PAL ")
         if(splitArray.length > 1){
-            addConsoleIfNoExist(splitArray[1],myData[i])    
+            addConsoleIfNoExist(splitArray[1],MyData[i])    
         }else{
-            addConsoleIfNoExist(splitArray[0], myData[i])    
+            addConsoleIfNoExist(splitArray[0], MyData[i])    
         }   
     }
     displayGames()
@@ -30,7 +30,7 @@ function addConsoleIfNoExist(pointerConsole, currentGame){
         allConsoles.push(pointerConsole)
         displayObject[pointerConsole] = currentGame["price-in-pennies"]
     }else{
-        for (var i = 0; i < allConsoles.length; i++) {
+        for (let i = 0; i < allConsoles.length; i++) {
             if (allConsoles[i] === pointerConsole) {
                 //get current price add to it then set the new price
                 currentPrice = displayObject[pointerConsole]
@@ -38,6 +38,7 @@ function addConsoleIfNoExist(pointerConsole, currentGame){
                 displayObject[pointerConsole] = newPrice
                 break
             } else if (i === allConsoles.length - 1) {
+                //it doesnt exist yet
                 allConsoles.push(pointerConsole)
                 displayObject[pointerConsole] = currentGame["price-in-pennies"]
             }
@@ -49,60 +50,74 @@ function displayGames(){
     //first of all set the right prices
     totalCost = changePricesToDollars()
     //then make a canvas
-    var canvas = document.createElement("canvas")
+    let canvas = document.createElement("canvas")
     attributes = {
         id: ["myCanvas"],
-        width: [(canvasWidth + "px")],
-        height: [(canvasHeight + "px")],
+        width: [(CanvasWidth + "px")],
+        height: [(CanvasHeight + "px")],
         style: ["border:2px solid #000000"]
     }
     canvas = setElementAttribute(canvas, attributes)
     addTextToCanvas(canvas, totalCost)
-    addGraph(canvas)
+    addGraph(canvas,totalCost)
     document.body.appendChild(canvas)
 }
 
 function addGraph(canvas, totalCost){
     let widthOfGraphCanvas = 1400
     let heightOfGraphCanvas = 700
+    let widthBetweenBars = 10
     let widthOffset = 200
     let ctx = canvas.getContext("2d");
     ctx.beginPath()
     ctx.lineWidth = "2";
     ctx.strokeStyle = "Red";
-    ctx.rect(widthOffset, 0, widthOfGraphCanvas, heightOfGraphCanvas)
+    ctx.rect(widthOffset, 0, widthOfGraphCanvas + widthBetweenBars, heightOfGraphCanvas)
     ctx.stroke()
     //get the width of the canvas and split it up by the length of allConsoles
-    addBars(widthOfGraphCanvas,ctx)
+    addBars(widthOfGraphCanvas,widthBetweenBars,totalCost,ctx)
     
 }
 
-function addBars(widthOfGraphCanvas,ctx){
+function addBars(widthOfGraphCanvas, widthBetweenBars, totalCost, ctx){
     let widthOfBars = widthOfGraphCanvas / allConsoles.length
-    let widthBetweenBars = 10
-    let startLeft = 225
+    let startLeft = 140
     let startHeight = 650
     let heightIncrement = 10
-    for(var i = 0; i < allConsoles.length; i++){
+    let highestPercentage = {
+        consoleName = allConsoles[0],
+        percent = displayObject[allConsoles[0]]
+    }
+    let allPercentages = []
+    for (let i = 0; i < allConsoles.length; i++) {
+        let percentage = displayObject[allConsoles[i]] / (totalCost / 100)
+        allPercentages[allConsoles[i]] = percentage
+        if(percentage > highestPercentage){
+            highestPercentage = {
+                consoleName = allConsoles[i],
+                percentage = percentage
+            }
+        }
+    }
+    //the biggest graph has to have 25 BELLOW the top bar
+    let percentageHeightOfGraph = highestPercentage.percentage + (highestPercentage.percentage / 4)
+    for(let i = 0; i < allConsoles.length; i++){
+        //percentage = price from console / 1 percent of total cost (1 percent = total cost/100)
+        let percentage = allPercentages[allConsoles[i]]
         ctx.beginPath()
         ctx.fillStyle = "Red";
-        ctx.fillRect(startLeft, 50, widthOfBars - widthBetweenBars ,startHeight)
+        ctx.fillRect(startLeft + widthOfBars, 50, widthOfBars - widthBetweenBars ,startHeight)
         startLeft += widthOfBars
         startHeight += heightIncrement
         ctx.stroke()
     }
-   
 }
 
 function addTextToCanvas(canvas, totalCost){
-    var ctx = canvas.getContext("2d");
+    let ctx = canvas.getContext("2d");
     ctx.font = "20px Arial";
     ctx.fillText(("$" + totalCost), 10, 790);
     ctx.beginPath()
-//     ctx.lineWidth = "2";
-//     ctx.strokeStyle = "Black";
-//     ctx.rect(200, 0, 1400, 700)
-//     ctx.stroke()
 }
 
 function changePricesToDollars(){
@@ -125,7 +140,7 @@ function getNZD(){
 
 function setElementAttribute(element, attributes) {
     for (key in attributes) {
-        for (var i = 0; i < attributes[key].length; i++) {
+        for (let i = 0; i < attributes[key].length; i++) {
             element.setAttribute(key, attributes[key][i])
         }
     };
